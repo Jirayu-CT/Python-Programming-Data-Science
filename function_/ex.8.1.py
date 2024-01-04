0e70623fedfeb4b9409efac647238ccb7a2db9a2
Lucibel <125994181+Jirayu-CT@users.noreply.github.com>
2024-01-04 23:55:05 +0700

    2/1/2024

--- function_/ex.8.1.py
+++ function_/ex.8.1.py
@@ -1,12 +1,12 @@
-def input_num():
-    num_list = []
-    for i in range(4):
-        num = int(input(f"Enter Number{i+1}: "))
-        num_list.append(num)
+# def input_num():
+#     num_list = []
+#     for i in range(4):
+#         num = int(input(f"Enter Number{i+1}: "))
+#         num_list.append(num)
 
-    return num_list
+#     return num_list
 
-def avg(num_list):
+def avg(*num_list):
     sum = 0
     for i in range(len(num_list)):
         sum += num_list[i]
@@ -14,4 +14,5 @@ def avg(num_list):
     return sum / len(num_list)
 
 
-print(avg(input_num()))
+# print(avg(input_num()))
+print(avg(4, 5, 6, 7))
